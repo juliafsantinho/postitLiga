35755def9c2fb0a61ee99c48772940109e2250a3
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-07-16 15:00:13 -0300

    login

--- postit/src/app/pages/login/login.page.ts
+++ postit/src/app/pages/login/login.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { loginPayload } from 'src/app/models/payload/login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
   constructor() { }
 
-  ngOnInit() {
+  public loginPayload: loginPayload = {
+    email: '',
+    password: '',
   }
 
+  public login():void {
+    console.log(this.loginPayload)
+  }
+  
 }
