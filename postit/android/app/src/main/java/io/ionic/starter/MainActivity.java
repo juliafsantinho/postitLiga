3d581e3c175d6b94079c0fc0c5c0ae18769ca04d
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-08-12 16:37:41 -0300

    feat(postit): Apk of this project with Capacitor

--- postit/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ postit/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,13 @@
 package io.ionic.starter;
 
+
+
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+
+  }
+
+
